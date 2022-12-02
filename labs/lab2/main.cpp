8b32540a6b82811a28147cce713729c48a919951
TreeAtOr <gdorevino@gmail.com>
2022-12-02 17:06:34 +0300

    add build files

--- labs/lab2/main.cpp
+++ labs/lab2/main.cpp
@@ -1,10 +1,6 @@
-#include <windows.h>
 #include "menu/MenuOfProgram.h"
 
 int main() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-
     MenuOfProgram* menu = new MenuOfProgram;
     menu->startProgram();
 
--- makefile
+++ makefile
@@ -3,3 +3,7 @@ lab1:
 	if [ ! -d build ];  then mkdir build; fi
 	mv labs/lab1/lab1 build;
 	
+lab2:
+	cd labs/lab2 && make;
+	if [ ! -d build ];  then mkdir build; fi
+	mv labs/lab2/lab2 build;
